b5b6cb3f0276016b216059902ea444f8ba0289e9
matt-gallery <matt.r.gallery@gmail.com>
2025-04-24 10:15:04 -0400

    updated cconnection to front end

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,6 @@ import outfitController from '../controllers/outfitController.js';
 
 const router = Router();
 
-// Route for getting clothing recommendations based on weather
-router.post('/recommendations', outfitController.getWeatherBasedRecommendations);
 
 // GET route for weather-based clothing recommendations
 router.get('/recommendations/:userId', outfitController.getWeatherRecommendationsGet);
